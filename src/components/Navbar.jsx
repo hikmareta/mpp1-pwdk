6fdbda93e045e40a82f7c175eb663a48208b23ca
hikmareta <andhika1499@gmail.com>
2023-09-01 11:02:41 +0700

    Feat: Adding Navigation Bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,23 @@ import style from "../style/Navbar.module.css";
 
 export const Navbar = () => {
   return (
-    <nav className = {style.navbar}>
-      <div className = {style.leftSec}> Left Section </div>
-      <div className = {style.rightSec}> Right Section </div>
+    <nav className={style.navbar}>
+      <div className={style.leftSec}>
+        <h1>
+          <b> PASSIFY </b>
+        </h1>
+      </div>
+      <div className={style.middleSec}>
+        <input type="text" placeholder="Search Event" className = {style.searchBar}/>
+      </div>
+      <div className={style.rightSec}>
+        <p> Create Event </p>
+        <p> Explore </p>
+        <div>
+          <button className={style.loginBtn}> Login </button>
+          <button className={style.registerBtn}> Sign Up </button>
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
