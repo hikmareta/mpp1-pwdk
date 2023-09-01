49404f50d1cfb39fc0c4236f0429d64c080920e5
Fadhil <79124345+fdhlnrzzmn@users.noreply.github.com>
2023-09-02 00:57:00 +0800

    style: foldering components

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../organisms/Navbar'
+import Hero from '../organisms/Hero'
+
+const Home = () => {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Hero></Hero>
+    </>
+  )
+}
+
+export default Home
