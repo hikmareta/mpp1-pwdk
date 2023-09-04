b3bec847055793b67d51e98d35b78eaad9fb62ca
hikmareta <andhika1499@gmail.com>
2023-09-04 20:51:43 +0700

    Feat: Adding new Login and Signup features

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,36 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/organisms/Navbar'
-import Footer from './components/organisms/Footer'
-import Home from './components/pages/Home'
-import NotFound from './components/organisms/NotFound'
-import EventDashboard from './components/pages/EventDashboard'
-import EventDashboardDetails from './components/pages/EventDashboardDetails'
-import OrderList from './components/pages/OrderList'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/organisms/Navbar";
+import Footer from "./components/organisms/Footer";
+import Home from "./components/pages/Home";
+import NotFound from "./components/organisms/NotFound";
+import EventDashboard from "./components/pages/EventDashboard";
+import EventDashboardDetails from "./components/pages/EventDashboardDetails";
+import OrderList from "./components/pages/OrderList";
+import Login from "./components/pages/Login";
+import Signup from "./components/pages/Signup";
+import ChooseRole from "./components/pages/ChooseRole";
 
 function App() {
   return (
     <>
-    <Navbar/ >
-    <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/:id/dashboard' element={<EventDashboard />} />
-        <Route path='/:id/dashboard/:eventID' element={<EventDashboardDetails />} />
-        <Route path='/:id/order-list' element={<OrderList/>} />
-        <Route path='*' element={<NotFound />} />
-    </Routes> 
-    <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id/dashboard" element={<EventDashboard />} />
+        <Route
+          path="/:id/dashboard/:eventID"
+          element={<EventDashboardDetails />}
+        />
+        <Route path="/:id/order-list" element={<OrderList />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/choose-role" element={<ChooseRole />} />
+      </Routes>
+      <Footer />
     </>
-   )
+  );
 }
 
-export default App
+export default App;
