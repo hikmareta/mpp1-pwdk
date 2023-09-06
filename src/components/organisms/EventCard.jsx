41856da7ca8e14d13575e47e3ea1f7190d19d1c4
hilmi <wijaksanahilmi@gmail.com>
2023-09-07 00:41:32 +0700

    edit:format tailwind

--- src/components/organisms/EventCard.jsx
+++ src/components/organisms/EventCard.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import styles from "../../style/EventDashboardDetails.module.css";
 const EventCard = () => {
   return (
-    <div className={styles.eventCard}>
+    <div className="flex w-full flex-col overflow-hidden bg-white shadow-lg rounded-xl">
       <img
+        className="w-full object-cover h-[50%]"
         src="https://img.freepik.com/free-vector/music-event-poster-template-with-abstract-shapes_1361-1316.jpg"
         alt=""
       />
-      <div style={{ padding: "1em" }}>
-        <p>IDR 1.000.000</p>
-        <p>Event Title Lorem Ipsum is simply dummy text of</p>
+      <div className="flex flex-col p-4 gap-y-2">
+        <p className="font-medium text-primaryColor text-xl">IDR 1.000.000</p>
+        <p className="font-semibold text-black">Event Title Lorem Ipsum is simply dummy text of</p>
         <p>01 Sept 2023</p>
         <p>19.00 - 22.00 WIB</p>
         <p>Jakarta, DKI Jakarta</p>
--- src/components/pages/EventDashboardDetails.jsx
+++ src/components/pages/EventDashboardDetails.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "../../style/EventDashboardDetails.module.css";
 import { BackButton } from "../atoms/BackButton";
 import Chart from "../molecules/Chart";
 import EventCard from "../organisms/EventCard";
@@ -54,46 +53,48 @@ const EventDashboardDetails = () => {
   return (
     <Container>
       <BackButton>Event Dashboard Details</BackButton>
-      <div className={styles.section1}>
-        <div className={styles.section1Left}>
-          <div className={styles.chartWrapper}>
+      <div className="flex w-full gap-4 mb-8 max-md:flex-wrap">
+        <div className="flex flex-col w-[65%] min-h-[40%] max-h-fit justify-between items-center max-md:w-full">
+          <div className="w-full h-[90%] bg-white p-2 rounded-xl shadow-lg max-md:h-[300px]">
             <Chart data={data} />
           </div>
-          <div className={styles.btnWrapper}>
-            <button className={styles.btnPrimary}>Edit Event</button>
-            <button className={styles.btnDanger}>Delete Event</button>
+          <div className="flex w-full justify-end gap-x-4 mt-4 max-md:justify-center">
+            <button className="bg-primaryColor text-white font-normal py-1 px-4 rounded">Edit Event</button>
+            <button className="bg-transparent hover:bg-red-700 text-red-700 font-normal hover:text-white py-1 px-4 border border-red-700 hover:border-transparent rounded">Delete Event</button>
           </div>
         </div>
-        <div className={styles.section1Right}>
+        <div className="flex flex-col w-[35%] gap-y-4 min-h-[45%] max-md:w-full max-h-fit h-fit">
           <EventCard />
           <PromoCard onClickAddPromo={() => setModalOpen(true)} />
         </div>
       </div>
-      <h2 style={{ fontSize: "1.5em", fontWeight: "600" }}>Transaction</h2>
-        <table className={styles.table}>
+      <h2 className="text-2xl font-semibold">Transaction</h2>
+      <div className='block overflow-x-scroll'>
+        <table className="table-auto w-full">
           <thead>
             <tr>
-              <th>Date</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Qty</th>
-              <th>Promo Code</th>
-              <th>Price</th>
-              <th>Total</th>
+              <th className="px-4 py-2 text-start">Date</th>
+              <th className="px-4 py-2 text-start">Name</th>
+              <th className="px-4 py-2 text-start">Email</th>
+              <th className="px-4 py-2 text-start">Qty</th>
+              <th className="px-4 py-2 text-start">Promo Code</th>
+              <th className="px-4 py-2 text-start">Price</th>
+              <th className="px-4 py-2 text-start">Total</th>
             </tr>
           </thead>
           <tbody>
             <tr>
-              <td>1 Sept 23</td>
-              <td>Lorem Ipsum</td>
-              <td>LoremIpsum@gmail.com</td>
-              <td>3</td>
-              <td>-</td>
-              <td>IDR 1.000.000</td>
-              <td>IDR 3.000.000</td>
+              <td className="px-4 py-2 text-start w-30">1 Sept 23</td>
+              <td className="px-4 py-2 text-start">Lorem Ipsum</td>
+              <td className="px-4 py-2 text-start">LoremIpsum@gmail.com</td>
+              <td className="px-4 py-2 text-start">3</td>
+              <td className="px-4 py-2 text-start">-</td>
+              <td className="px-4 py-2 text-start">IDR 1.000.000</td>
+              <td className="px-4 py-2 text-start">IDR 3.000.000</td>
             </tr>
           </tbody>
         </table>
+        </div>
       {modalOpen && <ModalPromo onClickCancel={() => setModalOpen(false)} />}
     </Container>
   );
